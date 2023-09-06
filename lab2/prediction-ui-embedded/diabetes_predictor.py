59df472d94879312bca9958ea321938a564613bb
IndikaKuma <ipkumarawd@yahoo.com>
2023-09-06 22:14:59 +0200

    Add Initial content for Docker Lab (Lab 2)

---
+++ lab2/prediction-ui-embedded/diabetes_predictor.py
@@ -0,0 +1,17 @@
+import json
+
+import pandas as pd
+from keras.models import load_model
+
+
+class DiabetesPredictor:
+    def __init__(self):
+        self.model = None
+
+    def predict_single_record(self, df):
+        if self.model is None:
+            self.model = load_model('model.h5')
+        y_pred = self.model.predict(df)
+        print(y_pred[0])
+        status = (y_pred[0] > 0.5)
+        return status
