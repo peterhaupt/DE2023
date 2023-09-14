b011111e913a25be4788041ae162eaef3cd70c99
IndikaKuma <ipkumarawd@yahoo.com>
2023-09-14 11:53:16 +0200

    Update docker.sh
    Update docker sh

--- installation_script/docker.sh
+++ installation_script/docker.sh
@@ -11,7 +11,7 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Add the repository to Apt sources:
+# Install Docker packages  and run the hello world container:
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
