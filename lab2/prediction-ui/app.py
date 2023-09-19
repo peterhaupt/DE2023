c974aef91fe33df89da54555c9b5ce4eba5b0ec3
IndikaKuma <ipkumarawd@yahoo.com>
2023-09-19 23:18:00 +0200

    Update to use port from $PORT env var - need for Cloud RUN

--- lab2/prediction-ui/app.py
+++ lab2/prediction-ui/app.py
@@ -53,4 +53,4 @@ def check_diabetes():
 # The code within this conditional block will only run the python file is executed as a
 # script. See https://realpython.com/if-name-main-python/
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
