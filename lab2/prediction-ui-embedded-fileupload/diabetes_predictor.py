649df2f91189a517968858edd5682c9ae339bc82
IndikaKuma <ipkumarawd@yahoo.com>
2023-09-28 14:31:47 +0200

    Updated Live Lab example 2

--- lab2/prediction-ui-embedded-fileupload/diabetes_predictor.py
+++ lab2/prediction-ui-embedded-fileupload/diabetes_predictor.py
@@ -1,14 +1,14 @@
+import logging
+
 from keras.models import load_model
 
 
 class DiabetesPredictor:
-    def __init__(self):
-        self.model = None
+    def __init__(self, model_file):
+        self.model = load_model(model_file)
 
-    def predict_single_record(self, df):
-        if self.model is None:
-            self.model = load_model('model.h5')
+    def predict_single_record(self, df, model_file='model.h5'):
         y_pred = self.model.predict(df)
-        print(y_pred[0])
+        logging.info("Prediction Output : %s", y_pred[0])
         status = (y_pred[0] > 0.5)
         return status
--- lab2/prediction-ui-embedded-fileupload/tests/test_01_diabetes_predictor.py
+++ lab2/prediction-ui-embedded-fileupload/tests/test_01_diabetes_predictor.py
@@ -1,4 +1,5 @@
-# content of test_sysexit.py
+import os
+
 import pandas as pd
 
 # content of test_class.py
@@ -7,9 +8,13 @@ import diabetes_predictor
 
 class TestDiabetesPredictor:
     def test_predict_single_record(self):
-        with open('../testResources/prediction_request.json') as json_file:
+        test_dir = os.path.dirname(os.path.abspath(__file__))
+        test_data_file = os.path.join(test_dir, "../testResources/prediction_request.json")
+        test_model_file = os.path.join(test_dir, "../testResources/model.h5")
+        with open(test_data_file) as json_file:
             data = pd.read_json(json_file)
-        dp = diabetes_predictor.DiabetesPredictor()
+        dp = diabetes_predictor.DiabetesPredictor(model_file=test_model_file)
         status = dp.predict_single_record(data)
+        print(os.getcwd())
         assert bool(status[0]) is not None
         assert bool(status[0]) is False
